556c8dfef0918244c6bc9a2c0fc220bda9bd30a2
Alejandro <asuarez@iri.upc.edu>
2020-12-15 18:36:35 +0100

    code further simplified

--- 2020/15/part1.cpp
+++ 2020/15/part1.cpp
@@ -4,19 +4,16 @@ using namespace std;
 
 // reminiscent of the van eck sequence: https://oeis.org/A181391
 
+const unsigned i_requested = 2020;
+
 int main() {
-  vector<int> most_recent_appearance;
+  vector<int> most_recent_appearance(i_requested,-1);
   unsigned i = 0;
   unsigned a_i;
-  while (cin >> a_i) {
-    if (most_recent_appearance.size() <= a_i)
-      most_recent_appearance.resize(a_i+1,-1);
+  while (cin >> a_i)
     most_recent_appearance[a_i] = i++;
-  }
 
-  while (i < 2020) {
-    if (most_recent_appearance.size() <= a_i)
-      most_recent_appearance.resize(a_i+1,-1);
+  while (i < i_requested) {
     int last_appearance = most_recent_appearance[a_i];
     most_recent_appearance[a_i] = i-1;
     a_i = last_appearance==-1? 0 : i - last_appearance - 1;
