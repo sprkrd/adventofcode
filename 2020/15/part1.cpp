b2dc5982bf156122760d982f3ddf3f7669153f25
Alejandro <asuarez@iri.upc.edu>
2020-12-15 18:25:42 +0100

    improved code

--- 2020/15/part1.cpp
+++ 2020/15/part1.cpp
@@ -5,31 +5,24 @@ using namespace std;
 // reminiscent of the van eck sequence: https://oeis.org/A181391
 
 int main() {
-  vector<int> last_occurrence;
+  vector<int> most_recent_appearance;
   unsigned i = 0;
-  unsigned last,second_to_last;
-  while (cin >> last) {
-    if (i > 0) {
-      if (last_occurrence.size() <= second_to_last)
-        last_occurrence.resize(second_to_last+1,-1);
-      last_occurrence[second_to_last] = i-1;
-    }
-    second_to_last = last;
-    ++i;
+  unsigned a_i;
+  while (cin >> a_i) {
+    if (most_recent_appearance.size() <= a_i)
+      most_recent_appearance.resize(a_i+1,-1);
+    most_recent_appearance[a_i] = i++;
   }
 
   while (i < 2020) {
-    second_to_last = last;
-    if (last_occurrence.size() <= second_to_last)
-      last_occurrence.resize(second_to_last+1,-1);
-    if (last_occurrence[second_to_last] == -1)
-      last = 0;
-    else
-      last = i - last_occurrence[second_to_last] - 1;
-    last_occurrence[second_to_last] = i-1;
+    if (most_recent_appearance.size() <= a_i)
+      most_recent_appearance.resize(a_i+1,-1);
+    int last_appearance = most_recent_appearance[a_i];
+    most_recent_appearance[a_i] = i-1;
+    a_i = last_appearance==-1? 0 : i - last_appearance - 1;
     ++i;
   }
 
-  cout << last << endl;
+  cout << a_i << endl;
 }
 
