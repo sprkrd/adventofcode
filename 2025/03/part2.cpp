6380c47bfb0859a8e33ecc3dbac58916c0fd20fe
sprkrd <hemar.tej@gmail.com>
2025-12-03 23:17:33 +0100

    Solve problem 3 in O(n)

--- 2025/03/part2.cpp
+++ 2025/03/part2.cpp
@@ -1,7 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr int kDigits = 12;
+constexpr size_t kDigits = 12;
+
+uint64_t MaxJoltage(const string& bank)
+{
+    vector<int> digits;
+    size_t remainingBatteries = bank.size();
+    for (char c : bank)
+    {
+        --remainingBatteries;
+        int digit = c-'0';
+        while (!digits.empty() && digit>digits.back() && remainingBatteries>=kDigits-digits.size())
+        {
+            digits.pop_back();
+        }
+        if (digits.size() < kDigits)
+        {
+            digits.push_back(digit);
+        }
+    }
+    return accumulate(digits.begin(), digits.end(), 0ULL, [](uint64_t acc, int digit)
+    {
+        return acc*10 + digit;
+    });
+}
 
 int main()
 {
@@ -9,14 +32,7 @@ int main()
     string line;
     while (getline(cin, line))
     {
-        uint64_t jolts = 0;
-        auto begin = line.begin();
-        for (auto end = line.end()-kDigits+1; end <= line.end(); ++end)
-        {
-            auto digitIt = max_element(begin, end);
-            jolts = jolts*10 + (*digitIt-'0');
-            begin = digitIt+1;
-        }
+        uint64_t jolts = MaxJoltage(line);
         ans += jolts;
     }
     cout << ans << endl;
